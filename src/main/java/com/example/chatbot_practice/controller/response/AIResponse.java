bc1eb35b92aa0015dfecfafe687119ec3ff24476
JSH-631 <newred02@handong.ac.kr>
2025-08-01 19:21:56 +0900

    erase unused files

--- src/main/java/com/example/chatbot_practice/controller/response/AIResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.chatbot_practice.controller.response;
-
-public record AIResponse(
-        boolean result,
-        boolean hasTime,
-        String datetime,
-        String content
-) {}
